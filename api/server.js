498265df8a781faf9ae03e34a5c4f481e48931bc
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-02-09 17:42:12 -0500

    refactored the server apis

--- api/server.js
+++ api/server.js
@@ -1,19 +1,33 @@
 const express = require('express')
+const games = require('../models/gamesModel')
 
 const server = express();
 
 server.use(express.json());
-const games = [{title: "Red Dead Redemption", genre: "RPG", releaseYear: 2018}, {title: "God of War", genre: "RPG", releaseYear: 2018}]
 
 server.get('/games', async (req, res) => {
-  res.status(200).json(games)
+  const allGames = await games.getAllGames();
+
+  res.status(200).json(allGames)
+})
+
+server.get('/games/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const game = await games.getGameById(id)
+  res.status(200).json(game)
 })
 
 server.post('/games', async (req, res) => {
   const body = req.body;
 
   if(body.title && body.genre) {
-    res.status(201).json({success: "the game has been added"})
+    try {
+      await games.insert(body)
+      res.status(201).json({success: "the game has been added"})
+    } catch (error) {
+      res.status(500).json({failure: "unable to send data"})
+    }
   } else {
     res.status(422).json({failure: "please add the title or genre"})
   }
